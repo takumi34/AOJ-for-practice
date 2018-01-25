2e8eb4b07fb457e66bed2e987cf33b04662303fa
Takumi <34294333+takumi34@users.noreply.github.com>
2018-01-25 18:56:15 +0900

    Update 0004_Simultaneous_Equation.py

--- Volume 0/0004_Simultaneous_Equation.py
+++ Volume 0/0004_Simultaneous_Equation.py
@@ -5,7 +5,7 @@ import sys
 for i in sys.stdin:
     a,b,c,d,e,f = map(float,i.split())
     g = a*e - b*d
-    if(g != 0):　#連立方程式の解が一意に存在するという問題設定なのでなくても可
+    if g != 0:　#連立方程式の解が一意に存在するという問題設定なのでなくても可
         x = (c*e-b*f)/g
         y = (a*f-c*d)/g  
         print("{:.3f} {:.3f}".format(x+0,y+0))
