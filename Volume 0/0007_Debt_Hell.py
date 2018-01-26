8ddf60c8618891a07ad897942e2327f612a7ba97
Takumi <34294333+takumi34@users.noreply.github.com>
2018-01-26 19:34:34 +0900

    Create 0007_Debt_Hell.py

---
+++ Volume 0/0007_Debt_Hell.py
@@ -0,0 +1,10 @@
+#問題 http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=0007
+
+import math
+N = int(input())
+a = 0
+b = 100000
+while a < N:
+    b = (math.ceil(b*1.05/1000))*1000
+    a += 1
+print(b)
